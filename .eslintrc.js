4961e5ff3678033b746b952a1f1519af196ae372
mbright90 <michael.bright90@icloud.com>
2023-02-22 10:58:44 +0000

    Implement eslint compatibility with jsx

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,43 @@
 module.exports = {
-    env: {
-        browser: true,
-        commonjs: true,
-        es6: true,
-        jest: true,
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jest/recommended',
+    'plugin:testing-library/react',
+    'plugin:prettier/recommended'
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    'babel-options': {
+      presets: ['@babel/preset-react']
     },
-    extends: [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:jsx-a11y/recommended",
-        "plugin:react-hooks/recommended",
-        "plugin:jest/recommended",
-        "plugin:testing-library/react",
-        'plugin:prettier/recommended'
-    ],
-    parser: "babel-eslint",
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true
-        },
-        ecmaVersion: 2018,
-        sourceType: "module"
+    ecmaFeatures: {
+      jsx: true
     },
-    plugins: [],
-    root: true,
-    rules: {
-        "indent": ["error", 2],
-        "no-console": "off",
-        "quotes": ["error", "single"],
-        "react/react-in-jsx-scope": "off",
-        "semi": ["error", "never"],
-        "prettier/prettier": [
-            "error", { "endOfLine": "off" }
-        ]
-    },
-    settings: {
-        react: {
-            version: "detect"
-        }
+    ecmaVersion: 2018,
+    requireConfigFile: false,
+    sourceType: 'module'
+  },
+  root: true,
+  rules: {
+    indent: ['error', 2],
+    'no-console': 'off',
+    quotes: ['error', 'single'],
+    'react/react-in-jsx-scope': 'off',
+    semi: ['error', 'never']
+  },
+  settings: {
+    react: {
+      version: 'detect'
     }
+  }
 }
--- craco.config.js
+++ craco.config.js
@@ -1,27 +1,28 @@
 const path = require('path')
 
 module.exports = {
-    eslint: {
-        rules: {
-            "indent": ["error", 2],
-            "no-console": "off",
-            "quotes": ["error", "single"],
-            "react/react-in-jsx-scope": "off",
-            "semi": ["error", "never"],
-            "prettier/prettier": [
-                "error",
-                {
-                    "endOfLine": "off"
-                }
-            ]
-        }
-    },
-    webpack: {
-        alias: {
-            '@app': path.resolve(__dirname, './src/app'),
-            '@assets': path.resolve(__dirname, './src/assets'),
-            '@images': path.resolve(__dirname, './src/assets/images'),
-            '@components': path.resolve(__dirname, './src/components')
-        }
+  eslint: {
+    rules: {
+      indent: ['error', 2],
+      'no-console': 'off',
+      quotes: ['error', 'single'],
+      'react/react-in-jsx-scope': 'off',
+      semi: ['error', 'never']
     }
+  },
+  webpack: {
+    alias: {
+      '@app': path.resolve(__dirname, './src/app'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@images': path.resolve(__dirname, './src/assets/images'),
+      '@components': path.resolve(__dirname, './src/components')
+    },
+    module: {
+      rules: {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [{ loader: 'babel-loader' }]
+      }
+    }
+  }
 }
