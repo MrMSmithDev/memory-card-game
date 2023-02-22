d712d4ec534ec2005ac2924e4c485ec3b87ee2cd
mbright90 <michael.bright90@icloud.com>
2023-02-22 10:59:20 +0000

    Normalize app file and implement css reset

--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,13 @@
 import '@app'
 
+import './App.module.css'
+
 function App() {
   return (
     <div className="App">
-
+      <p>Welcome to the galaxy</p>
     </div>
   )
 }
 
-export default App;
+export default App
