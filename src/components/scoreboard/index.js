101e6abf775af9bcfc8d33c03bb34151b2a9d0ff
mbright90 <michael.bright90@icloud.com>
2023-02-23 13:57:37 +0000

    Add scoreboard coloring and comparison

--- src/components/scoreboard/index.js
+++ src/components/scoreboard/index.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import { appContext } from '@app'
+import React, { useContext } from 'react'
 
 import style from './Scoreboard.module.scss'
 
 const Scoreboard = () => {
-  const score = 5
-  const topScore = 10
+  const { currentScore, bestScore } = useContext(appContext)
 
   return (
     <div className={style.scoreboard}>
-      <p>Score: {score}</p>
-      <p>Best: {topScore}</p>
+      <p style={currentScore === 0 ? { color: '#E90064' } : null}>Score: {currentScore}</p>
+      <p>Best: {bestScore}</p>
     </div>
   )
 }
