14b7d983240384af1c63ecb229cdd6522e201e36
mbright90 <michael.bright90@icloud.com>
2023-02-23 13:41:30 +0000

    Add randomised card settings

--- src/components/cardGrid/index.js
+++ src/components/cardGrid/index.js
@@ -4,19 +4,37 @@ import React, { useContext } from 'react'
 
 import style from './CardGrid.module.scss'
 
+const createRandomIndexes = () => {
+  // Create array from 0 - 11, to indicate indexes of characters
+  const array = Array.from({ length: 12 }, (_, i) => i)
+
+  for (let i = array.length - 1; i > 0; i -= 1) {
+    // Choose a random index to swap the current index with
+    const newI = Math.floor(Math.random() * (i + 1))
+
+    // Swap the two values over. Semi-colon separates it from the
+    // initialization in the previous expression
+    ;[array[i], array[newI]] = [array[newI], array[i]]
+  }
+  return array
+}
+
 const CardGrid = () => {
   const { characterArr } = useContext(appContext)
 
-  const createCardSet = (arr) =>
-    arr.map((character) => (
+  const createCardSet = (indexArray) => {
+    return indexArray.map((randomIndex) => (
       <Card
-        character={character.characterName}
-        imageUrl={character.imageUrl}
-        key={character.identifier}
+        character={characterArr[randomIndex].characterName}
+        identifier={characterArr[randomIndex].identifier}
+        imageUrl={characterArr[randomIndex].imageUrl}
+        key={characterArr[randomIndex].identifier}
+        tabIndex={indexArray.findIndex((number) => number === randomIndex) + 1}
       />
     ))
+  }
 
-  const cardSet = createCardSet(characterArr)
+  const cardSet = createCardSet(createRandomIndexes())
 
   return <div className={style.cardGrid}>{cardSet}</div>
 }
