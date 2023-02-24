93d6387144dc7e930dc7768f283442c3ee35e2ab
mbright90 <michael.bright90@icloud.com>
2023-02-24 09:44:28 +0000

    Update tests for Footer

--- src/components/footer/Footer.test.js
+++ src/components/footer/Footer.test.js
@@ -1,9 +1,10 @@
+import Footer from '@components/app'
 import { render, screen } from '@testing-library/react'
+import React from 'react'
 
-import Footer from './index'
-
-test('renders learn react link', () => {
+test('Renders footer link with correct text', () => {
   render(<Footer />)
-  const linkElement = screen.getByText(/MBright90/i)
-  expect(linkElement).toBeInTheDocument()
+  const footerLink = screen.getByText(/mBright90/i)
+  expect(footerLink).toBeInTheDocument()
+  expect(footerLink.href).toBe('https://github.com/MBright90/memory-card-game')
 })
