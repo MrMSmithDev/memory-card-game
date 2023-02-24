60084fe27a6fa677744272cb754d6356e01ebe82
mbright90 <michael.bright90@icloud.com>
2023-02-24 10:02:25 +0000

    Add img role to div for testing purposes

--- src/components/cardImage/index.js
+++ src/components/cardImage/index.js
@@ -4,7 +4,13 @@ import React from 'react'
 import style from './CardImage.module.scss'
 
 const CardImage = (props) => {
-  return <div className={style.cardImage} style={{ backgroundImage: `url(${props.imageUrl})` }} />
+  return (
+    <div
+      className={style.cardImage}
+      style={{ backgroundImage: `url(${props.imageUrl})` }}
+      role="img"
+    />
+  )
 }
 
 CardImage.propTypes = {
