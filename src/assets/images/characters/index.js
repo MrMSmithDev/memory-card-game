ea72bdfb5a1b484dfcb0f567179c684707f97194
mbright90 <michael.bright90@icloud.com>
2023-02-25 09:50:23 +0000

    Restructure character image files

---
+++ src/assets/images/characters/index.js
@@ -0,0 +1,29 @@
+import darthMaulImg from './darth-maul-card.jpg'
+import darthVaderImg from './darth-vader-card.jpg'
+import finnImg from './finn-card-jpg'
+import hanSoloImg from './han-solo-card.jpg'
+import kyloRenImg from './kylo-ren-card.jpg'
+import lukeSkywalkerImg from './luke-skywalker-card.jpg'
+import obiWanImg from './obi-wan-card.jpg'
+import padmeImg from './padme-card.jpg'
+import palpatineImg from './palpatine-card.jpg'
+import princessLeiaImg from './princess-leia-card.jpg'
+import reyImg from './rey-card.jpg'
+import yodaImg from './yoda-card.jpg'
+
+const images = {
+  darthMaul: darthMaulImg,
+  darthVader: darthVaderImg,
+  finn: finnImg,
+  hanSolo: hanSoloImg,
+  kyloRen: kyloRenImg,
+  lukeSkywalker: lukeSkywalkerImg,
+  obiWan: obiWanImg,
+  padme: padmeImg,
+  palpatine: palpatineImg,
+  princessLeia: princessLeiaImg,
+  rey: reyImg,
+  yoda: yodaImg
+}
+
+export default images
