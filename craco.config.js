58d5542862f2880481cc8e003df3c7fe9c8f4801
mbright90 <michael.bright90@icloud.com>
2023-02-25 09:49:27 +0000

    Add image optimization plugin and config

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin')
 
 module.exports = {
   eslint: {
@@ -21,7 +22,7 @@ module.exports = {
       '@app': path.resolve(__dirname, './src/app'),
       '@assets': path.resolve(__dirname, './src/assets'),
       '@fonts': path.resolve(__dirname, './public/static/fonts'),
-      '@images': path.resolve(__dirname, './public/static/images'),
+      '@characters': path.resolve(__dirname, './src/assets/images/characters'),
       '@components': path.resolve(__dirname, './src/components'),
       '@styles': path.resolve(__dirname, './src/globalStyles')
     },
@@ -31,6 +32,43 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         use: [{ loader: 'babel-loader' }]
       }
+    },
+    optimization: {
+      minimizer: [
+        '...',
+        new ImageMinimizerPlugin({
+          minimizer: {
+            implementation: ImageMinimizerPlugin.imageminMinify,
+            options: {
+              plugins: [
+                ['gifsicle', { interlaced: true }],
+                ['jpegtran', { progressive: true }],
+                ['optipng', { optimizationLevel: 5 }],
+                [
+                  'svgo',
+                  {
+                    plugins: [
+                      {
+                        name: 'preset-default',
+                        params: {
+                          overrides: {
+                            removeViewBox: false,
+                            addAttributesToSVGElement: {
+                              params: {
+                                attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }]
+                              }
+                            }
+                          }
+                        }
+                      }
+                    ]
+                  }
+                ]
+              ]
+            }
+          }
+        })
+      ]
     }
   }
 }
