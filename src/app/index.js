d61fa3dd00d8a41b6f85a165aa16016a3bcfa492
mbright90 <michael.bright90@icloud.com>
2023-02-25 09:51:33 +0000

    Add character image imports and implement

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,5 @@
 import appBackground from '@assets/images/background.jpg'
+import images from '@characters'
 import CardGrid from '@components/cardGrid/index'
 import Footer from '@components/footer'
 import Header from '@components/header'
@@ -9,82 +10,81 @@ import style from './App.module.scss'
 
 export const appContext = createContext()
 
-const characterArr = [
-  {
-    characterName: 'Darth Maul',
-    identifier: 'darthMaul',
-    imageUrl: 'static/images/characters/darth-maul-card.jpg',
-    isChosen: false
-  },
-  {
-    characterName: 'Darth Vader',
-    identifier: 'darthVader',
-    imageUrl: 'static/images/characters/darth-vader-card.jpg',
-    isChosen: false
-  },
-  {
-    characterName: 'Finn FN-2187',
-    identifier: 'finnFn2187',
-    imageUrl: 'static/images/characters/finn-card.jpg',
-    isChosen: false
-  },
-  {
-    characterName: 'Han Solo',
-    identifier: 'hanSolo',
-    imageUrl: 'static/images/characters/han-solo-card.jpg',
-    isChosen: false
-  },
-  {
-    characterName: 'Kylo Ren',
-    identifier: 'kyloRen',
-    imageUrl: 'static/images/characters/kylo-ren-card.jpg',
-    isChosen: false
-  },
-  {
-    characterName: 'Luke Skywalker',
-    identifier: 'lukeSkywalker',
-    imageUrl: 'static/images/characters/luke-skywalker-card.jpg',
-    isChosen: false
-  },
-  {
-    characterName: 'Obi-Wan Kenobi',
-    identifier: 'obiWanKenobi',
-    imageUrl: 'static/images/characters/obi-wan-card.jpg',
-    isChosen: false
-  },
-  {
-    characterName: 'Padme Amidala',
-    identifier: 'padme',
-    imageUrl: 'static/images/characters/padme-card.jpg',
-    isChosen: false
-  },
-  {
-    characterName: 'Emperor Palpatine',
-    identifier: 'emperorPalpatine',
-    imageUrl: 'static/images/characters/palpatine-card.jpg',
-    isChosen: false
-  },
-  {
-    characterName: 'Princess Leia',
-    identifier: 'princessLeia',
-    imageUrl: 'static/images/characters/princess-leia-card.jpg',
-    isChosen: false
-  },
-  {
-    characterName: 'Rey Skywalker',
-    identifier: 'reySkywalker',
-    imageUrl: 'static/images/characters/rey-card.jpg',
-    isChosen: false
-  },
-  {
-    characterName: 'Yoda',
-    identifier: 'yoda',
-    imageUrl: 'static/images/characters/yoda-card.jpg',
-    isChosen: false
-  }
-]
-
 const AppProvider = ({ children }) => {
+  const [characterArr, setCharacterArr] = useState([
+    {
+      characterName: 'Darth Maul',
+      identifier: 'darthMaul',
+      imageUrl: images.darthMaul,
+      isChosen: false
+    },
+    {
+      characterName: 'Darth Vader',
+      identifier: 'darthVader',
+      imageUrl: images.darthVader,
+      isChosen: false
+    },
+    {
+      characterName: 'Finn FN-2187',
+      identifier: 'finnFn2187',
+      imageUrl: images.finn,
+      isChosen: false
+    },
+    {
+      characterName: 'Han Solo',
+      identifier: 'hanSolo',
+      imageUrl: images.hanSolo,
+      isChosen: false
+    },
+    {
+      characterName: 'Kylo Ren',
+      identifier: 'kyloRen',
+      imageUrl: images.kyloRen,
+      isChosen: false
+    },
+    {
+      characterName: 'Luke Skywalker',
+      identifier: 'lukeSkywalker',
+      imageUrl: images.lukeSkywalker,
+      isChosen: false
+    },
+    {
+      characterName: 'Obi-Wan Kenobi',
+      identifier: 'obiWanKenobi',
+      imageUrl: images.obiWan,
+      isChosen: false
+    },
+    {
+      characterName: 'Padme Amidala',
+      identifier: 'padme',
+      imageUrl: images.padme,
+      isChosen: false
+    },
+    {
+      characterName: 'Emperor Palpatine',
+      identifier: 'emperorPalpatine',
+      imageUrl: images.palpatine,
+      isChosen: false
+    },
+    {
+      characterName: 'Princess Leia',
+      identifier: 'princessLeia',
+      imageUrl: images.princessLeia,
+      isChosen: false
+    },
+    {
+      characterName: 'Rey Skywalker',
+      identifier: 'reySkywalker',
+      imageUrl: images.rey,
+      isChosen: false
+    },
+    {
+      characterName: 'Yoda',
+      identifier: 'yoda',
+      imageUrl: images.yoda,
+      isChosen: false
+    }
+  ])
   const [currentScore, setCurrentScore] = useState(0)
   const [bestScore, setBestScore] = useState(0)
 
@@ -94,8 +94,10 @@ const AppProvider = ({ children }) => {
   const isDuplicateChoice = (charIdentifier) =>
     characterArr[findCharacterIndex(charIdentifier)].isChosen
 
-  const addCharacterClicked = (charIdentifier) =>
-    (characterArr[findCharacterIndex(charIdentifier)].isChosen = true)
+  const addCharacterClicked = (charIdentifier) => {
+    characterArr[findCharacterIndex(charIdentifier)].isChosen = true
+    setCharacterArr([...characterArr])
+  }
 
   const resetCharacterArr = () => {
     characterArr.forEach((character) => {
