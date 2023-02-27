8884900f16af92c209e5448a8ff1674d7e9c765a
mbright90 <michael.bright90@icloud.com>
2023-02-27 16:22:50 +0000

    Initiate and create HelpLink component

---
+++ src/components/helpLink/index.js
@@ -0,0 +1,32 @@
+import { faCircleInfo } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome/index'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+import style from './HelpLink.module.scss'
+
+const HelpLink = () => {
+  const [isActive, setIsActive] = useState(false)
+
+  const handleLinkClick = () => {
+    isActive ? setIsActive(false) : setIsActive(true)
+  }
+
+  const handleLinkKeyDown = (e) => {
+    if (e.code === 'Enter') handleLinkClick()
+  }
+
+  return (
+    <div
+      className={`${style.helpLink} ${isActive ? style.active : null}`}
+      onClick={handleLinkClick}
+      onKeyDown={handleLinkKeyDown}
+    >
+      <Link to={isActive ? '/' : '/help'}>
+        <FontAwesomeIcon icon={faCircleInfo} />
+      </Link>
+    </div>
+  )
+}
+
+export default HelpLink
