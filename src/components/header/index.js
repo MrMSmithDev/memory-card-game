43ee55eb689ea96db28d3094612f36ada0fe697b
mbright90 <michael.bright90@icloud.com>
2023-02-27 16:20:51 +0000

    Add HelpLink component import

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,3 +1,4 @@
+import HelpLink from '@components/helpLink'
 import Scoreboard from '@components/scoreboard'
 import React from 'react'
 
@@ -6,6 +7,7 @@ import style from './Header.module.scss'
 const Header = () => {
   return (
     <header className={style.container}>
+      <HelpLink />
       <h1 className={style.siteTitle}>MIND TRAINING</h1>
       <Scoreboard />
     </header>
