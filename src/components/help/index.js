065f70b11ed0f91b440ba9a43621ee4c24de21ea
mbright90 <michael.bright90@icloud.com>
2023-02-28 10:04:57 +0000

    Add information and link to play game

--- src/components/help/index.js
+++ src/components/help/index.js
@@ -1,7 +1,41 @@
-import React from 'react'
+import { appContext } from '@app'
+import React, { useContext } from 'react'
+import { Link } from 'react-router-dom'
+
+import style from './Help.module.scss'
 
 const Help = () => {
-  return <h1>HELP!</h1>
+  const { setIsHelpActive } = useContext(appContext)
+
+  const handlePlayClick = () => {
+    setIsHelpActive(false)
+  }
+
+  return (
+    <div className={style.container}>
+      <h1 className={style.helpTitle}>How to Play</h1>
+      <p className={style.infoPara}>
+        You will see <span className={style.emphasized}>twelve</span> character cards. You must
+        choose one of the cards. Try to remember which card you chose, as each time you choose a
+        card, the cards will be shuffled into a random order. Choose another character card that is
+        different from the one you chose before. You{' '}
+        <span className={style.emphasized}>cannot</span> choose the same card twice.
+      </p>
+      <p className={style.infoPara}>
+        Repeat these steps until you have chosen all 12 character cards without repeating any
+        choices. If the you successfully choose all 12 different cards without making any duplicate
+        choices, you win the game. If you make a duplicate choice, your score is reset to zero and
+        you must start again from the beginning.
+      </p>
+      <p className={style.infoPara}>
+        Most importantly...
+        <span className={style.emphasized}>Have fun!</span>
+      </p>
+      <div className={style.playButton} onClick={handlePlayClick} onKeyDown={handlePlayClick}>
+        <Link to="/">Play</Link>
+      </div>
+    </div>
+  )
 }
 
 export default Help
