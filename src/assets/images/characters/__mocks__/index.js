99b360263613a3e8c6ba6ae57120b883bae93318
mbright90 <michael.bright90@icloud.com>
2023-03-23 12:57:23 +0000

    Add image mocks

---
+++ src/assets/images/characters/__mocks__/index.js
@@ -0,0 +1,14 @@
+export default {
+  darthMaul: 'darthMaulImg',
+  darthVader: 'darthVaderImg',
+  finn: 'finnImg',
+  hanSolo: 'hanSoloImg',
+  kyloRen: 'kyloRenImg',
+  lukeSkywalker: 'lukeSkywalkerImg',
+  obiWan: 'obiWanImg',
+  padme: 'padmeImg',
+  palpatine: 'palpatineImg',
+  princessLeia: 'princessLeiaImg',
+  rey: 'reyImg',
+  yoda: 'yodaImg'
+}
